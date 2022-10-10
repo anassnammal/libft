7796b2af2cf3e558cded26cc4d67d9a8917cf884
anass nammal <anassnammal@gmail.com>
2022-10-10 23:48:52 +0100

    done

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,17 +14,14 @@
 
 void *ft_memchr(const void *s, int c, size_t n)
 {
-	size_t i;
-	unsigned char *str;
-	unsigned char chr;
+	int i;
 
 	i = 0;
-	str = (unsigned char *) s;
-	chr = (unsigned char) c;
 	while (i < n)
 	{
-		if (*(str + i) == chr)
-			return (str + i);
+		if (*(unsigned char *)(s + i) == c)
+			return ((void *)s + i);
+		i++;
 	}
 	return (0);
 }
