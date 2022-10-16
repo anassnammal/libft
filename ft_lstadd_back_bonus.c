e506328f5792d6cb5e6dc99c86110edc68c65f14
anass nammal <anassnammal@gmail.com>
2022-10-16 17:02:12 +0100

    done

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -7,7 +7,7 @@ void ft_lstadd_back(t_list **lst, t_list *new)
 		while (*lst)
 			*lst = *lst->next;
 		new->next = 0;
-		*lst
+		*lst 
 	}
 	return (NULL);
 }
--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -3,5 +3,5 @@
 void ft_lstadd_front(t_list **lst, t_list *new)
 {
 	new->next = *lst;
-	*lst = new;
+	*lst = new; 
 }
