a57e76e6daa6b0de51e0a2e2b356a94a2a5039a3
anass-nam <anassnammal@gmail.com>
2022-10-21 08:58:05 -0400

    done

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -11,6 +11,22 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+#include "libft.h"
+
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *newlist;
+
+	if (lst != NULL && f != NULL)
+	{
+		newlist = f(lst);
+		if (newlist != NULL && lst->next != NULL)
+			newlist->next = ft_lstmap(lst->next, f);
+		return (newlist);
+	}
+	return (NULL);
+}
+
 // t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 // {
 // 	t_list *new;
