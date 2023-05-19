6e958a122c43f9a2385c85f3161b4887d473559c
Anass Nammal <anassnammal@gmail.com>
2023-05-19 22:40:27 +0100

    updated

--- ft_atoi.c
+++ ft_atoi.c
@@ -20,23 +20,20 @@ int	ft_atoi(const char *str)
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
 	s = 1;
-	if (*str == '-' && ft_isdigit(*(char *)(str + 1)))
-	{
-		s = -1;
-		str++;
-	}
-	else if (*str == '+' && ft_isdigit(*(char *)(str + 1)))
-		str++;
+	if ((*str == '-' || *str == '+')&& ft_isdigit(*(char *)(str + 1)))
+		s = 44 - *(str++);
 	r = 0;
 	p = 0;
+	if (ft_strlen(str-1) >= 11 && !ft_isdigit(*(str + 10)) && ft_strncmp(str-1, "-2147483648", 11) == 0)
+		return (-2147483648);
 	while (ft_isdigit(*str))
 	{
-		if (r < p && s < 0)
-			return (0);
-		else if (r < p)
-			return (-1);
+		if (r < p)
+			break ;
 		p = r;
 		r = r * 10 + ((*str++) - 48);
 	}
+	if (r < p)
+		return ((s == -1) - 1);
 	return (r * s);
 }
